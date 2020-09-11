d13343e4bd57de8384654e92b03d9246fe61399e
villedarony91 <villedarony91>
2020-09-10 23:02:53 -0600

    landing theotokos elearning

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-spacer />
+      <v-btn text>¿Quiénes Somos?</v-btn>
+      <v-btn text>¿Qué Hacemos?</v-btn>
+      <v-btn text>¿Cómo lo hacemos?</v-btn>
+      <v-btn text>Contacto</v-btn>
+      <v-btn text>Soy Theotoko</v-btn>
+      <v-spacer />
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <nuxt />
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,228 @@
+<template>
+  <v-main style="padding: 0">
+    <section id="hero">
+      <v-row no-gutters>
+        <v-img
+          :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+          src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
+        >
+          <v-theme-provider dark>
+            <v-container fill-height>
+              <v-row
+                align="center"
+                class="white--text mx-auto"
+                justify="center"
+              >
+                <v-col class="white--text text-center" cols="12" tag="h1">
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-3' : 'display-4',
+                    ]"
+                    class="font-weight-black"
+                    >E-learning Theotokos</span
+                  >
+                </v-col>
+
+                <v-btn
+                  class="align-self-end"
+                  fab
+                  outlined
+                  @click="$vuetify.goTo('#about-me')"
+                >
+                  <v-icon>mdi-chevron-double-down</v-icon>
+                </v-btn>
+              </v-row>
+            </v-container>
+          </v-theme-provider>
+        </v-img>
+      </v-row>
+    </section>
+
+    <section id="about-me">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">¿Quiénes Somos?</h2>
+
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-responsive
+          class="mx-auto title font-weight-light mb-8"
+          max-width="720"
+        >
+          Vuetify is the #1 component library for Vue.js and has been in active
+          development since 2016. The goal of the project is to provide users
+          with everything that is needed to build rich and engaging web
+          applications using the Material Design specification. It accomplishes
+          that with a consistent update cycle, Long-term Support (LTS) for
+          previous versions, responsive community engagement, a vast ecosystem
+          of resources and a dedication to quality components.
+        </v-responsive>
+
+        <v-avatar class="elevation-12 mb-12" size="128">
+          <v-img src alt="logo"></v-img>
+        </v-avatar>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <section id="features" class="grey lighten-3">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">Nuestro Equipo</h2>
+
+        <v-responsive class="mx-auto mb-12" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-row>
+          <v-col
+            v-for="({ icon, title, text }, i) in features"
+            :key="i"
+            cols="12"
+            md="4"
+          >
+            <v-card class="py-12 px-4" color="grey lighten-5" flat>
+              <v-theme-provider dark>
+                <div>
+                  <v-avatar color="primary" size="88">
+                    <v-icon large v-text="icon"></v-icon>
+                  </v-avatar>
+                </div>
+              </v-theme-provider>
+
+              <v-card-title
+                class="justify-center font-weight-black text-uppercase"
+                v-text="title"
+              ></v-card-title>
+
+              <v-card-text class="subtitle-1" v-text="text"></v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <section id="about-me">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">¿Qué Hacemos?</h2>
+
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-responsive
+          class="mx-auto title font-weight-light mb-8"
+          max-width="720"
+        >
+          Vuetify is the #1 component library for Vue.js and has been in active
+          development since 2016. The goal of the project is to provide users
+          with everything that is needed to build rich and engaging web
+          applications using the Material Design specification. It accomplishes
+          that with a consistent update cycle, Long-term Support (LTS) for
+          previous versions, responsive community engagement, a vast ecosystem
+          of resources and a dedication to quality components.
+        </v-responsive>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <section id="about-me">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">¿Cómo lo Hacemos?</h2>
+
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-responsive
+          class="mx-auto title font-weight-light mb-8"
+          max-width="720"
+        >
+          Vuetify is the #1 component library for Vue.js and has been in active
+          development since 2016. The goal of the project is to provide users
+          with everything that is needed to build rich and engaging web
+          applications using the Material Design specification. It accomplishes
+          that with a consistent update cycle, Long-term Support (LTS) for
+          previous versions, responsive community engagement, a vast ecosystem
+          of resources and a dedication to quality components.
+        </v-responsive>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <v-sheet id="contact" color="#333333" dark tag="section" tile>
+      <div class="py-12"></div>
+
+      <v-container>
+        <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
+          Contact Me
+        </h2>
+
+        <v-responsive class="mx-auto mb-12" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-theme-provider light>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field flat label="Name*" solo></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field flat label="Email*" solo></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field flat label="Subject*" solo></v-text-field>
+            </v-col>
+
+            <v-col cols="12">
+              <v-textarea flat label="Message*" solo></v-textarea>
+            </v-col>
+
+            <v-col class="mx-auto" cols="auto">
+              <v-btn color="accent" x-large>Submit</v-btn>
+            </v-col>
+          </v-row>
+        </v-theme-provider>
+      </v-container>
+
+      <div class="py-12"></div>
+    </v-sheet>
+  </v-main>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+}
+</script>
